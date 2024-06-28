c911ed21dc496502d7e4a0ed7b2441bd9f58d31f
Kyle Cupples <kyle.cupples@gmail.com>
2024-06-27 19:42:24 -0700

    SuperUpdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
-(function($) {
+jQuery.noConflict();
+$(document).ready(function() {
 
 	var	$window = $(window),
 		$body = $('body'),
@@ -26,6 +26,143 @@
 				$body.removeClass('is-preload');
 			}, 100);
 		});
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 50) {
+				$('#nav').addClass('scrolled');
+			} else {
+				$('#nav').removeClass('scrolled');
+			}
+		});
+	// Smooth scrolling for internal links
+	$('a[href^="#"]').on('click', function(event) {
+		var target = $(this.getAttribute('href'));
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top
+			}, 1000);
+		}
+	});
+	//typewriter
+	document.addEventListener('DOMContentLoaded', function() {
+		var app = document.getElementById('typewriter');
+	
+		var typewriter = new Typewriter(app, {
+			loop: true,
+			delay: 75,
+		});
+	
+		typewriter
+			.typeString('Real-time Feedback')
+			.pauseFor(1000)
+			.deleteAll()
+			.typeString('Exclusive Case Studies')
+			.pauseFor(1000)
+			.deleteAll()
+			.typeString('Peer Support')
+			.pauseFor(1000)
+			.deleteAll()
+			.typeString('Expert Insights')
+			.pauseFor(1000)
+			.start();
+	});
+	var TxtRotate = function(el, toRotate, period) {
+		this.toRotate = toRotate;
+		this.el = el;
+		this.loopNum = 0;
+		this.period = parseInt(period, 10) || 2000;
+		this.txt = '';
+		this.tick();
+		this.isDeleting = false;
+	  };
+	  
+	  TxtRotate.prototype.tick = function() {
+		var i = this.loopNum % this.toRotate.length;
+		var fullTxt = this.toRotate[i];
+	  
+		if (this.isDeleting) {
+		  this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+		  this.txt = fullTxt.substring(0, this.txt.length + 1);
+		}
+	  
+		this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+	  
+		var that = this;
+		var delta = 300 - Math.random() * 100;
+	  
+		if (this.isDeleting) { delta /= 2; }
+	  
+		if (!this.isDeleting && this.txt === fullTxt) {
+		  delta = this.period;
+		  this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === '') {
+		  this.isDeleting = false;
+		  this.loopNum++;
+		  delta = 500;
+		}
+	  
+		setTimeout(function() {
+		  that.tick();
+		}, delta);
+	  };
+	  
+	  window.onload = function() {
+		var elements = document.getElementsByClassName('txt-rotate');
+		for (var i=0; i<elements.length; i++) {
+		  var toRotate = elements[i].getAttribute('data-rotate');
+		  var period = elements[i].getAttribute('data-period');
+		  if (toRotate) {
+			new TxtRotate(elements[i], JSON.parse(toRotate), period);
+		  }
+		}
+		// INJECT CSS
+	// 	var css = document.createElement("style");
+	// 	css.type = "text/css";
+	// 	css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #64FFDA }";
+	// 	document.body.appendChild(css);
+	//   };
+	// Form validation
+	$('#signup-form').on('submit', function(e) {
+		e.preventDefault();
+		
+		var name = $('#name').val();
+		var email = $('#email').val();
+		var category = $('#category').val();
+
+		if (name && email && category) {
+			// Form is valid, you can submit it here
+			console.log('Form submitted:', { name, email, category });
+			alert('Thank you for signing up!');
+			this.reset();
+		} else {
+			alert('Please fill out all required fields.');
+		}
+	});
+
+	// Intersection Observer for fade-in animations
+	if ('IntersectionObserver' in window) {
+		const faders = document.querySelectorAll('.fade-in');
+		const appearOptions = {
+			threshold: 0.5,
+			rootMargin: "0px 0px -100px 0px"
+		};
+
+		const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+			entries.forEach(entry => {
+				if (!entry.isIntersecting) {
+					return;
+				} else {
+					entry.target.classList.add('appear');
+					appearOnScroll.unobserve(entry.target);
+				}
+			});
+		}, appearOptions);
+
+		faders.forEach(fader => {
+			appearOnScroll.observe(fader);
+		});
+	}
 
 	// Nav.
 		var $nav = $('#nav');
